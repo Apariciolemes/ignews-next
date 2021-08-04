4df5f5eb69170a59e04860f6c3adedf54a37b78f
Aparicio Lemes Neto <aparicio.neto@betalearning.com.br>
2021-08-03 23:19:04 -0300

    added Static Site Generation

---
+++ src/services/stripe.ts
@@ -0,0 +1,11 @@
+import Stripe from 'stripe'
+import { version } from '../../package.json'
+const envStripe = process.env.STRIPE_API_KEY
+
+export const stripe = new Stripe(envStripe, {
+    apiVersion: '2020-08-27',
+    appInfo: {
+        name: 'Ignews',
+        version,
+    },
+})
